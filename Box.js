3f6c27048fc2f52571da6f64b00e05084e4e0015
SanthyaVignesh <74100644+SanthyaVignesh@users.noreply.github.com>
2021-05-17 19:42:36 +0530

    Add files via upload

--- Box.js
+++ Box.js
@@ -1,20 +1,20 @@
 class Box{
-    constructor(x,y,w,h){
+    constructor(x,y,width,height){
         var option = {
-            restitution : 0.75
+            restitution : 1
         }
 
-        this.body = Bodies.rectangle(x,y,w,h,option);
-        this.width = w;
-        this.height = h;
-
+        this.body = Bodies.rectangle(x,y,width,height,option);
+        this.width = width;
+        this.height = height;
         World.add(world,this.body);
     }
 
     display(){
-        var pos = this.body.position;
-        rectMode(CENTER);
-        rect(pos.x,pos.y,this.width,this.height);
-    }
+        //help us to display the box
+        var pos = this.body.position
 
+        rectMode(CENTER);
+        rect(pos.x,pos.y,this.width,this.height)
+    }
 }
